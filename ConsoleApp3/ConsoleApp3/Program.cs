c40183d03249dbbbfcc0e16a27faa57a2603f532
eminemin02 <mustafayevemin153@gmail.com>
2024-10-15 12:51:16 +0400

    Class

---
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,79 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car1 = new Car("mercedes" , "e60" , 3240 , 4);
+            car1.ShowFullInfo();
+            Car car2 = new Car("bmw", "F10", 3240, 4);
+            car2.ShowFullInfo();
+
+            Moto moto1 = new Moto("tufan", "uy37", 1211, 2);
+            moto1.ShowFullInfo();
+            Moto moto2 = new Moto("tufan", "ik837", 9811, 2);
+            moto1.ShowFullInfo();
+        }
+    class Vehicle
+        {
+            public string Brand;
+            public string Model;
+            public double Mile;
+            public Vehicle(string brand, string model, double mile)
+            {
+                if ( brand==" " || model==" ")
+                {
+                    Console.WriteLine("Brand and model must enter");
+                }
+                Brand = brand;
+                Model = model;
+                Mile = mile;
+
+
+
+
+            }
+            public void ShowFullInfo()
+            {
+                Console.WriteLine($"Brand:{Brand},Model:{Model},Mile:{Mile}");
+            }
+
+        }
+    class Car : Vehicle
+        {
+            public int DoorNum;
+
+            public Car( string model, string brand, double mile, int doorNum) : base(model, brand, mile)
+            {
+                DoorNum = doorNum;
+            }
+            public void ShowFullInfo()
+            {
+                base.ShowFullInfo();
+                Console.WriteLine("doorNum : " + DoorNum);
+
+            }
+
+        }
+    class Moto: Vehicle
+        {
+            public int WhellNum;
+
+            public Moto( string model, string brand, double mile, int whellNUm) : base(model, brand, mile)
+            {
+                WhellNum = whellNUm;
+            }
+            public void ShowFullInfo()
+            {
+                base.ShowFullInfo();
+                Console.WriteLine("whellNum :" + WhellNum);
+
+            }
+
+        }
+
+
+    }
+}
---
+++ ConsoleApp4/ConsoleApp4/Program.cs
@@ -0,0 +1,70 @@
+﻿namespace cc
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Notebook Samsung = new Notebook("Samsung", "yaxsidi", 33, 2391, 8, 512);
+            Samsung.ShowFullData();
+            Notebook Asus = new Notebook("Asus", "eladi", 14, 1121, 8, 512);
+           Asus.ShowFullData();
+            Notebook Hp = new Notebook("Hp", "pisdi", 10, 1311, 8, 512);
+            Hp.ShowFullData();
+        }
+        class Product
+        {
+            public string Name;
+            public string Comment;
+            public int Count;
+            public bool IsStock = true;
+            public double Price;
+
+            public Product(string name, string comment, int count, double price)
+            {
+                Name = name;
+               Comment = comment;
+                Count = count;
+                Price = price;
+            }
+
+            public void Sale(string name, string comment, int count, double price)
+            {
+
+                Name = name;
+               Comment = comment;
+                Count = count;
+                Price = price;
+            }
+            public void ShowFullData()
+            {
+                Console.WriteLine("Name " + Name +  "Comment" + Comment + "Price" +   Price +    "Count"  + Count + "Price" + Price);
+            }
+
+        }
+        class Notebook : Product
+        {
+            public byte Ram;
+            public int Storage;
+            public Notebook(string name, string comment, int count, double price, byte ram, int storage) : base(name, comment, count, price)
+            {
+                if (string.IsNullOrEmpty(name) || count == ' ' || price == ' ' || ram == ' ')
+                {
+                    Console.WriteLine("verilen yoxdur");
+                }
+                else if (count == 0)
+                {
+                    Console.WriteLine("bitib");
+                }
+                Ram = ram;
+                Storage = storage;
+
+            }
+            public void ShowFullData()
+            {
+                base.ShowFullData();
+                Console.WriteLine("ram : " + Ram + "storage" + Storage);
+            }
+
+        }
+    }
+}
